88ff1da539cf92f388ccbdbbf7b5424f16b0b359
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-11-22 19:35:01 +0600

    first commit

---
+++ NodeJsCoreConcept/greetings.js
@@ -0,0 +1,18 @@
+const args = process.argv;
+
+// process.arg[0] = node path
+// process.arg[1] = file path
+// process.arg[2] = first actual argument
+
+const name = args[2] || "guest";
+const time = new Date().getHours();
+
+if (time < 12) {
+  greeting = "Good Morning";
+} else if (time < 18) {
+  greeting = "Good Afternoon";
+} else {
+  greeting = "Good Evening";
+}
+
+console.log(`${greeting} ${name}`);
